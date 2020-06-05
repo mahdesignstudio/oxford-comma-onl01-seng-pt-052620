5f66f43d8ce17a9778353f3c8d257882ead62a0c
mahdesignstudio <mahdesignstudio@gmail.com>
2020-06-04 21:10:15 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,10 +4,10 @@ def oxford_comma(array)
     elsif array.length == 2 
         return array.join(" and ") # lines 4-5: adds 'and' between elements when given a 2-element array
     else
-        array.length >= 3
+        array.length >= 3 #line 7: correctly formats arrays of lengths greater than three
         last_array = "and #{array[-1]}"
         array.pop
-        array.push(last_array)
-        return array.join(", ")
+        array.push(last_array) #lines 8-11: Adds commas plus a final 'and' when given a 3-element array
+        return array.join(", ") 
     end     
 end 
