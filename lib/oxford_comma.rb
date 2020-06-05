be5a2b3ea58e06d9d865945fc8a6c2877c3ac27a
mahdesignstudio <mahdesignstudio@gmail.com>
2020-06-04 21:07:28 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
-
-end
+def oxford_comma(array) 
+    if array.length == 1
+        return "#{array[0]}" # lines 2-3: returns a string without any additional formatting when given a 1-element array
+    elsif array.length == 2 
+        return array.join(" and ") # lines 4-5: adds 'and' between elements when given a 2-element array
+    else
+        array.length >= 3
+        last_array = "and #{array[-1]}"
+        array.pop
+        array.push(last_array)
+        return array.join(", ")
+    end     
+end 
